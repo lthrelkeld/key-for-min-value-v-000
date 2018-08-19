a00e2105a0e657f1d80afccb7762c20f8f119683
Elizabeth Karst <lkarst@outlook.com>
2018-08-19 17:47:15 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   name_hash.each { |key, value| starting_value = value }
-  min_value = starting_value
-  puts min_value
+  minimum_value = starting_value
+  puts minimum_value
 
   name_hash.each do |key, value|
-      if min_value < value
+      if minimum_value < value
         min_key = key
       else
         min_key
